2d888227d9a455e6926efd1eabe5b2dbe27a18b1
PraveenAnil <praveendosth@gmail.com>
2017-07-04 13:53:15 +0530

    Create linuxclientvmscript.sh
    Setup Linux Client

---
+++ scripts/linuxclientvmscript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## Argument 1 will be username for Client VMs  ##
+
+## To execute this script run sudo su -c'sh linuxclientvmscript.sh ClientVMsUsername'  ##
+
+# Allow sudo with out passoword
+echo "$1" "ALL = (ALL) NOPASSWD:ALL" >> /etc/sudoers
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh epel-release-latest-7*.rpm 
+systemctl stop firewalld
+systemctl disable firewalld
+
+
+exit 0
